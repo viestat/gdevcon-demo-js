275b488f2276dfb63f100f207e1573c81a1dc206
Andrés C. Viesca Ruiz <viestat@icloud.com>
2018-08-30 02:14:01 +0200

    Edit main app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './gdevcon.png';
+import ImageStep from './ImageStep';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The right tool for the job</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="App-main">
+          <ImageStep />
+        </main>
+        <footer>
+          <p>Made with ❤️ by <a href="https://github.com/viestat">@viestat</a></p>
+        </footer>
       </div>
     );
   }
